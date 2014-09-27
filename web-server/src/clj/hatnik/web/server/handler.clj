9d3bea2330c91f2cc020f804b5f3b4d2fe94beaf
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-09-27 11:45:33 +0100

    Add mock for /api/library-version method.

--- web-server/src/clj/hatnik/web/server/handler.clj
+++ web-server/src/clj/hatnik/web/server/handler.clj
@@ -11,18 +11,25 @@
             [hatnik.web.server.login :refer [login-api]]
             [hatnik.config :refer [config]]
 
+            [ring.util.response :as resp]
             [ring.middleware.json :as json]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.stacktrace :as stacktrace]))
 
 (timbre/set-level! (:log-level config))
 
+(defn library-version [group artifact]
+  (resp/response
+   {:result :ok
+    :version "1.6.0"}))
+
 (defroutes app-routes
   (GET "/" [] renderer/core-page)
   (context "/api" []
            (context "/projects" [] projects-api)
            (context "/actions" [] actions-api)
-           login-api)
+           login-api
+           (GET "/library-version" [group artifact] (library-version group artifact)))
   (route/resources "/")
   (route/not-found "Not Found"))
 

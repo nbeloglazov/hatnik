201b562ea0bc9206d85c28e5203e108a883cfe6c
Norgat <norg113@gmail.com>
2014-09-27 13:32:58 +0400

    Add project list accordions.

--- web-server/src/cljs/hatnik/web/client/project-list.cljs
+++ web-server/src/cljs/hatnik/web/client/project-list.cljs
@@ -5,27 +5,32 @@
             [hatnik.web.client.components :as widget]))
 
 (def app-state 
-  (atom {:num 0
-         :projects []}))
-
-
-(defn click-handler []
-  (swap! app-state
-         assoc :num
-         (+ 1 (:num @app-state))))
+  (atom {:projects []}))
 
 
 (defn project-list-widget [data owner]
   (reify
     om/IRender
     (render [this]
-      (widget/accordion-panel
-       :header "FOO project"
-       :body (dom/p nil "Foo project is good.")
-       :body-id "iFooProject"))))
+      (apply dom/div nil
+       (map 
+        (fn [prj]
+          (widget/accordion-panel
+           :header (get prj "name")
+           :body (dom/p nil "This project is good.")
+           :body-id (str "__" (get prj "name"))))
+        (:projects data))))))
 
 (om/root project-list-widget app-state
          {:target (. js/document (getElementById "iProjectList"))})
 
 
+(defn update-projects-list [reply]
+  (let [json (.getResponseJson (.-target reply))
+        data (js->clj json)]
+    (when (= "ok" (get data "result"))
+      (swap! app-state
+             assoc :projects
+             (get data "projects")))))
 
+(.send goog.net.XhrIo "/api/projects" update-projects-list)

6cd2fa6c0eab155dc187c5161a335e318b9717ed
Norgat <norg113@gmail.com>
2014-09-27 13:11:12 +0400

    Add Empty form for modal window.

--- web-server/src/cljs/hatnik/web/client/components.cljs
+++ web-server/src/cljs/hatnik/web/client/components.cljs
@@ -4,19 +4,21 @@
 
 
 (defn accordion-panel [& {:keys [body-id header body open] :or {:open true}}]
-  (dom/div #js {:className "panel panel-default"}
-           (dom/div #js {:className "panel-heading"}
-                    (dom/div nil
-                             (dom/h4 #js {:className "panel-title"}
-                                     (dom/a #js 
-                                            {:data-parent "#accrodion"
-                                             :data-toggle "collapse"
-                                             :href (str "#" body-id)} header))))
+  (dom/div 
+   #js {:className "panel panel-default"}
+   (dom/div 
+    #js {:className "panel-heading"}
+    (dom/div 
+     nil
+     (dom/h4 #js {:className "panel-title"}
+             (dom/a #js 
+                    {:data-parent "#accrodion"
+                     :data-toggle "collapse"
+                     :href (str "#" body-id)} header))))
            (dom/div #js {:className (if open 
                                       "panel-collapse collapse in"
                                       "panel-collapse collapse")
                          :id body-id}
                     (dom/div #js {:className "panel-body"}
                              body))))
-
 

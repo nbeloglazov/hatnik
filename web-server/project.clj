31b18d17cd4d3eb48ea381e3bd7f08c5b1102906
Norgat <norg113@gmail.com>
2014-09-27 10:11:51 +0400

    Fix web-server/project.cljs.

--- web-server/project.clj
+++ web-server/project.clj
@@ -4,8 +4,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2356"]
                  [compojure "1.1.8"]
-                 [om "0.7.3"]
-                 [racehub/om-bootstrap "0.3.0"]]
+                 [racehub/om-bootstrap "0.3.0"]
+                 [om "0.7.3"]]
 
   :plugins [[lein-ring "0.8.11"]
             [lein-cljsbuild "1.0.3"]]

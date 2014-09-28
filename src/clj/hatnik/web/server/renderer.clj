caa9af9ed07ca21833fc880d55405c6faa60a5d8
Norgat <norg113@gmail.com>
2014-09-28 13:15:47 +0400

    Add two versions of main page. Add login menu.

--- src/clj/hatnik/web/server/renderer.clj
+++ src/clj/hatnik/web/server/renderer.clj
@@ -14,77 +14,105 @@
        "client_id=" (:github-id config)
        "&scope=user:email"))
 
-(defn page-header []
+(defn modal-frames []
+  (hc/html
+   [:div#iModal.modal.fade {:data-keyboard "true"}
+    [:div#iModalDialog.modal-dialog
+     [:div.modal-content
+      [:div#iActionFormHeader.modal-header]
+      [:div#iActionFormBody.modal-body]
+      [:div#iActionFormFooter.modal-footer]]]]
+
+   [:div#iModalProject.modal.fade {:data-keyboard "true"}
+    [:div.modal-dialog
+     [:div.modal-content
+      [:div.modal-header
+       [:h4.modal-title "Creating a new project"]]
+      [:div.modal-body
+       [:form
+        [:input#project-name-input.form-control 
+         {:type "text"
+          :placeholder "Project name"}]]]
+      [:div.modal-footer
+       [:button.btn.btn-primary {:onClick "hatnik.web.client.z_actions.send_new_project_request()"} "Create"]]]]]
+
+   [:div#iModalProjectMenu.modal.fade 
+    [:div.modal-dialog
+     [:div.modal-content
+      [:div.modal-header
+       [:h4.modal-title "Project menu"]]
+      
+      [:div.modal-body
+       [:form
+        [:input#project-name-edit-input.form-control 
+         {:type "text"
+          :placeholder "Project name"}]]]
+
+      [:div.modal-footer
+       [:div.btn.btn-primary.pull-left
+        {:onClick "hatnik.web.client.z_actions.update_project()"}
+        "Update"]
+       [:div.btn.btn-danger.pull-right
+        {:onClick "hatnik.web.client.z_actions.delete_project()"}
+        "Delete"]]]]]))
+
+(defn page-menu [user]
   [:nav.navbar.navbar-default {:role "navigation"}
    [:div#navbarCollapse.collapse.navbar-collapse
-    [:ul.nav.navbar-nav.navbar-right
-     [:li
-      [:a.btn 
-       {:href github-link} "Login via GitHub"]]]]])
+    (if user
+                                        ; User log in
+      [:ul.nav.navbar-nav.navbar-right
+       [:li
+        [:a {:href "#"} (:email user)]]
+       [:li
+        [:a.btn {:href "/api/logout"} "Logout"]]]
+      
+                                        ; User don't log in
+      [:ul.nav.navbar-nav.navbar-right         
+       [:li
+        [:a.btn 
+         {:href github-link} "Login via GitHub"]]])]])
 
-(defn core-page [req]
+(defn work-main-page [req]
   (hc/html
-   (page-html-head)
-   
-   [:body
+   [:div.page-header
+    [:h2 "Project list"]]
 
-    [:div.container
-     (page-header)
-     [:div.page-header
-      [:h2 "Project list"]]
+   [:div.row      
+    [:div.col-md-2
+     [:a.btn.btn-success 
+      {:href "#" 
+       :onclick "$('#iModalProject').modal()"} "Create new"]]
+    [:div.col-md-10]]
+
+   [:div
+    [:p ""]]
+   [:div#iProjectList.panel-group "Loading..."]))
+
+(defn about-page []
+  [:div.page-header
+   [:h2 "Hatnik?"]])
+  
+(defn core-page [req]
+  (let [user (:user (:session req))]
+    (hc/html
+     (page-html-head)
      
-     [:div.row      
-      [:div.col-md-2
-       [:a.btn.btn-success 
-        {:href "#" 
-         :onclick "$('#iModalProject').modal()"} "Create new"]]
-      [:div.col-md-10]]
-     [:div
-      [:p ""]]
-
-     [:div#iProjectList.panel-group "Loading..."]]
-
-    [:div#iModal.modal.fade {:data-keyboard "true"}
-     [:div#iModalDialog.modal-dialog
-      [:div.modal-content
-       [:div#iActionFormHeader.modal-header]
-       [:div#iActionFormBody.modal-body]
-       [:div#iActionFormFooter.modal-footer]]]]
-
-    [:div#iModalProject.modal.fade {:data-keyboard "true"}
-     [:div.modal-dialog
-      [:div.modal-content
-       [:div.modal-header
-        [:h4.modal-title "Creating a new project"]]
-       [:div.modal-body
-        [:form
-         [:input#project-name-input.form-control 
-          {:type "text"
-           :placeholder "Project name"}]]]
-      [:div.modal-footer
-        [:button.btn.btn-primary {:onClick "hatnik.web.client.z_actions.send_new_project_request()"} "Create"]]]]]
-
-    [:div#iModalProjectMenu.modal.fade 
-     [:div.modal-dialog
-      [:div.modal-content
-       [:div.modal-header
-        [:h4.modal-title "Project menu"]]
+     [:body
+      [:div.container
+       (page-menu user)       
        
-       [:div.modal-body
-        [:form
-         [:input#project-name-edit-input.form-control 
-          {:type "text"
-           :placeholder "Project name"}]]]
+       (if user 
+         (work-main-page req)
+         (about-page))]
 
-       [:div.modal-footer
-        [:div.btn.btn-primary.pull-left
-         {:onClick "hatnik.web.client.z_actions.update_project()"}
-         "Update"]
-        [:div.btn.btn-danger.pull-right
-         {:onClick "hatnik.web.client.z_actions.delete_project()"}
-         "Delete"]]]]]
+       (when user (modal-frames))
 
-    [:script {:src "/js/jquery-2.1.1.min.js"}]    
-    [:script {:src "/js/bootstrap.min.js"}]
-    [:script {:src "http://fb.me/react-0.11.1.js"}]
-    [:script {:src "/gen/js/hatnik.js"}]]))
+      [:script {:src "/js/jquery-2.1.1.min.js"}]    
+      [:script {:src "/js/bootstrap.min.js"}]
+      [:script {:src "http://fb.me/react-0.11.1.js"}]
+      
+      (when user
+        [:script {:src "/gen/js/hatnik.js"}])
+])))
+

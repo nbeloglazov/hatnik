ca24a8764285a6ff5957f8737f93ce2fa039925f
Norgat <norg113@gmail.com>
2014-09-28 17:09:44 +0400

    Add submit email new action form by Enter pressing.

--- src/cljs/hatnik/web/client/form.cljs
+++ src/cljs/hatnik/web/client/form.cljs
@@ -2,7 +2,8 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [hatnik.web.client.form-components :as widget]
-            [hatnik.web.client.app-state :as state])
+            [hatnik.web.client.app-state :as state]
+            [hatnik.web.client.z-actions :as action])
   (:use [jayq.core :only [$]]))
 
 (om/root widget/action-form-header state/app-state
@@ -24,4 +25,10 @@
                (.modal ($ :#iModal) "hide")
                (.modal ($ :#iModalProject) "hide")
                (.modal ($ :#iModalProjectMenu) "hide"))))
+
+;; Submit new email action by Enter pressing
+(.keydown ($ :#iModal)
+          (fn [e]
+            (when (= 13 (.-keyCode e))
+              (action/send-new-email-action (:current-project (deref state/app-state))))))
 

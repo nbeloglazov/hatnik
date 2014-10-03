1f296ec4e01d96dcbd3be77282b1b1b57f730adb
Merlyn <curious.attempt.bunny@gmail.com>
2014-10-03 09:31:26 -0700

    Tweak the home page text.

--- src/clj/hatnik/web/server/renderer.clj
+++ src/clj/hatnik/web/server/renderer.clj
@@ -28,7 +28,7 @@
     [:div.modal-dialog
      [:div.modal-content
       [:div.modal-header
-       [:h4.modal-title "Creating a new project"]]
+       [:h4.modal-title "Create a new project"]]
       [:div.modal-body
        [:form
         [:input#project-name-input.form-control 
@@ -94,27 +94,27 @@
   [:div.about
    [:div.page-header
     [:h1 "Hatnik " [:small "Don't miss a release"]]
-    [:p "Hatnik is a web app that helps you to track library releases. Choose a library to track and setup an action that sends emails or creates github issues once library released."]]
+    [:p "Hatnik is a web app that helps you to track library releases. Choose a library to track and setup an action that sends emails or creates Github issues when a new release of the library is published."]]
 
    [:div.row
     [:div.col-md-6.col-xs-12.use-cases
      [:h3 "When to use"]
      [:p "Use cases might be different. Here is some of them:"]
      [:ul
-      [:li "You want to stay on the edge and use latest ClojureScript."]
+      [:li "You want to stay on the cutting edge and use the latest release of ClojureScript."]
       [:li "You keep forgetting to update wiki tutorials and examples "
-       "to use new version when you release a library."]
-      [:li "You want to have some place shows latest version of your favourite "
+       "to use the new version when you release your library."]
+      [:li "You want to have a place shows the latest version of your favourite "
        "libraries."]
-      [:li "You want your project to automatically switch to newer dependencies."]]]
+      [:li "You want your project to automatically update to the latest version of its dependencies."]]]
     [:div.col-md-6.col-xs-12.how-to
      [:h3 "How to start"]
      [:ol
       [:li "Login via GitHub. Sorry can't do without it."]
       [:li "Create and setup an action"
        [:ol
-        [:li "Choose library, for example " [:code "org.clojure/clojurescript"] "."]
-        [:li "Fill in action settings. For example send email with custom text."]]]]
+        [:li "Choose a library, for example " [:code "org.clojure/clojurescript"] "."]
+        [:li "Fill in the action settings. For example send an email with custom text."]]]]
      [:p "That's all! Now you forget about Hatnik until the library you're tracking is released. "
       "You'll get an email with your text. You can create multiple actions and group them by projects. "
       "A project is simply a group of actions related to each other."]]]
@@ -126,16 +126,16 @@
     "limited only to sending emails, but we're working on it! Here's the initial set of actions:"]
    [:ul.actions
     [:li.available [:strong  "Email"]
-     " - send email with custom text. You can use template variables to substitute library name or version."]
+     " - send an email with custom text. You can use template variables to substitute the library name or the version."]
     [:li.unavailable {:title "Work in progress"} [:strong "GitHub Issue"]
-     " - create an issue in selected repo. Text can be customized just like in email."]
+     " - create an issue in the selected repo. The text can be customized just like in email."]
     [:li.unavailable {:title "Work in progress"} [:strong "GitHub Pull Request"]
      " - create a pull request in selected repo using simple find-and-replace-in-file operations."]
     [:li.unavailable {:title "Work in progress"} [:strong "GitHub Wiki"]
-     " - change a wiki page using same find-and-replace operations."]
+     " - update a wiki page using the same find-and-replace operations."]
     [:li.unavailable {:title "Work in progress"} [:strong "Noop"]
      " - does nothing. Useful if you want to "
-     "have a place that shows latest versions for selected libraries."]]
+     "have a place that lists the latest versions for selected libraries."]]
    ])
 
 (defn core-page [req]

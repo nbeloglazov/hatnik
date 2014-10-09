a0e36b906cb56e8cd8c1512eb25a03a8343e9222
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-10-09 08:37:49 +0100

    Add tests for API.

---
+++ test/clj/hatnik/web/server/api_test.clj
@@ -0,0 +1,191 @@
+(ns hatnik.web.server.api-test
+  (:require [hatnik.web.server.handler :refer [map->WebServer]]
+            [hatnik.db.memory-storage :refer [map->MemoryStorage]]
+            [com.stuartsierra.component :as component]
+            [clojure.test :refer :all]
+            [clj-http.client :as c]
+            [taoensso.timbre :as timbre]
+            [hatnik.versions :as ver]
+            [clojure.data :refer [diff]]
+            [clj-http core cookies]))
+
+(def config
+  {:web {:port 6780}
+   :enable-force-login true
+   :db :memory})
+
+(def url (str "http://localhost:" (-> config :web :port) "/api"))
+
+(defn system-fixture [f]
+  (let [system (-> (component/system-map
+                    :config config
+                    :db (map->MemoryStorage {})
+                    :web-server (component/using
+                                 (map->WebServer {})
+                                 [:db :config]))
+                   (component/start))]
+    (try (f)
+         (finally
+           (component/stop system)))))
+
+(defn cookie-store-fixture [f]
+  (binding [clj-http.core/*cookie-store* (clj-http.cookies/cookie-store)]
+    (f)))
+
+(defn http [method path & [body]]
+  (let [resp (->> {:form-params body
+                   :content-type :json
+                   :accept :json
+                   :as :json
+                   :method method
+                   :url (str url path)
+                   :throw-exceptions false
+                   :coerce :always}
+                  c/request
+                  :body)]
+    (timbre/spy resp)))
+
+(use-fixtures :each system-fixture cookie-store-fixture)
+
+(defn ok? [resp]
+  (is (= (:result resp) "ok")
+      "Expected ok response")
+  resp)
+
+(defn error? [resp]
+  (is (= (:result resp) "error")
+      "Expected error response")
+  resp)
+
+(defn map-by-id [coll]
+  (into {} (map #(vector (keyword (:id %)) %) coll)))
+
+(defn assert-default-project [projects]
+  (is (= 1 (count projects)))
+  (let [[id project] (first projects)]
+    (is (= (name id) (:id project)))
+    (is (= (:name project) "Default"))
+    (is (empty? (:actions project)))))
+
+(defn data-equal [expected actual]
+  (let [[left right both] (diff expected actual)]
+    (is (nil? left))
+    (is (nil? right))))
+
+(deftest test-api-valid-usage
+  (let [quil-ver (-> (http :get "/library-version?library=quil")
+                     ok? :version)
+        ring-ver (-> (http :get "/library-version?library=ring")
+                     ok? :version)
+        email "me@email.com"
+
+        resp (ok? (http :get (str "/force-login?skip-dummy-data=true&email="
+                                  email)))
+
+        ; Check that default project is create for user
+        projects (-> (http :get "/projects") ok? :projects)
+        proj-dflt-id (-> projects first first name)
+        _ (data-equal projects
+                      (map-by-id [{:name "Default" :id proj-dflt-id
+                                   :actions {}}]))
+
+        ; Create project foo
+        proj-foo-id (-> (http :post "/projects" {:name "Foo"}) ok? :id)
+        expected [{:name "Default" :id proj-dflt-id :actions {}}
+                  {:name "Foo" :id proj-foo-id :actions {}}]
+        _ (data-equal (-> (http :get "/projects") ok? :projects)
+                      (map-by-id expected))
+
+        ;
+        ; Create 2 actions in Default proj and 1 action in Foo.
+        ;
+
+        ; Create first action in Default.
+        act-dflt-one {:project-id proj-dflt-id
+                      :type "email"
+                      :address email
+                      :template "Template dflt one"
+                      :library "quil"}
+        resp (ok? (http :post "/actions" act-dflt-one))
+        _ (is (= (:last-processed-version resp) quil-ver))
+        act-dflt-one (merge act-dflt-one (dissoc resp :result))
+
+        ; Create second action in Default.
+        act-dflt-two {:project-id proj-dflt-id
+                      :type "email"
+                      :address email
+                      :template "Template dflt two"
+                      :library "ring"}
+        resp (ok? (http :post "/actions" act-dflt-two))
+        _ (is (= (:last-processed-version resp) ring-ver))
+        act-dflt-two (merge act-dflt-two (dissoc resp :result))
+
+        ; Create single action in Foo.
+        act-foo-one {:project-id proj-foo-id
+                     :type "email"
+                     :address email
+                     :template "Template foo single"
+                     :library "quil"}
+        resp (ok? (http :post "/actions" act-foo-one))
+        _ (is (= (:last-processed-version resp) quil-ver))
+        act-foo-one (merge act-foo-one (dissoc resp :result))
+
+        ; Check that actions created correctly
+        expected [{:name "Default" :id proj-dflt-id
+                   :actions (map-by-id [act-dflt-one act-dflt-two])}
+                  {:name "Foo" :id proj-foo-id
+                   :actions (map-by-id [act-foo-one])}]
+        _ (data-equal (-> (http :get "/projects") ok? :projects)
+                      (map-by-id expected))
+
+        ; Rename project "Default" to "First"
+        _ (ok? (http :put (str "/projects/" proj-dflt-id) {:name "First"}))
+
+        ; Update action dflt-one. Change library and template.
+        act-dflt-one (assoc act-dflt-one
+                       :library "ring"
+                       :template "Oh my new template")
+        resp (->> (dissoc act-dflt-one :id :last-processed-version)
+                  (http :put (str "/actions/" (:id act-dflt-one)))
+                  ok?)
+        _ (data-equal {:result "ok" :last-processed-version ring-ver}
+                      resp)
+        act-dflt-one (merge act-dflt-one (dissoc resp :result))
+
+        ; Delete action dflt-two
+        _ (ok? (http :delete (str "/actions/" (:id act-dflt-two))))
+
+        ; Delete project "Foo"
+        _ (ok? (http :delete (str "/projects/" proj-foo-id)))
+
+        ; Check that only project "First" is present and it contains
+        ; single action.
+        expected [{:name "First" :id proj-dflt-id
+                   :actions (map-by-id [act-dflt-one])}]
+        _ (data-equal (-> (http :get "/projects") ok? :projects)
+                      (map-by-id expected))
+
+        ; Logout and check that we don't have access to projects.
+        _ (c/get (str url "/logout"))
+        resp (error? (http :get "/projects"))
+        ; Error response should have only :result and :message keys.
+        _ (is #{:result :message} (set (keys resp)))
+
+        ; Login as new user and check that we can't see previous user
+        ; project
+        resp (ok? (http :get (str "/force-login?skip-dummy-data=true&"
+                                  "email=new@email.com")))
+        projects (-> (http :get "/projects") ok? :projects)
+        proj-new-dflt-id (-> projects first first name)
+        _ (data-equal projects
+                      (map-by-id [{:name "Default" :id proj-new-dflt-id
+                                   :actions {}}]))
+
+        ; Login as old user again and verify that the project is
+        ; retrieved properly.
+        _ (ok? (http :get (str "/force-login?skip-dummy-data=true&email="                                  email)))
+        expected [{:name "First" :id proj-dflt-id
+                   :actions (map-by-id [act-dflt-one])}]
+        _ (data-equal (-> (http :get "/projects") ok? :projects)
+                      (map-by-id expected))
+        ]))

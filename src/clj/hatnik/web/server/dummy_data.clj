104cbeb63b6fba3e4182c22f1146f7affb6f3341
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-10-10 18:38:38 +0100

    Fix dummy data action types.
    When memory storage is used keywords are not converted to strings. And we currently expect only strings as action types.

--- src/clj/hatnik/web/server/dummy_data.clj
+++ src/clj/hatnik/web/server/dummy_data.clj
@@ -7,7 +7,7 @@
   (let [proj-id (stg/create-project! db {:name "Default"
                                          :user-id user-id})]
     (stg/create-action! db user-id {:project-id proj-id
-                                    :type :email
+                                    :type "email"
                                     :template "Hey {{LIBRARY}} released. New version {{VERSION}}."
                                     :address "nikelandjelo@gmail.com"
                                     :library "com.nbeloglazov/hatnik-test-lib"
@@ -17,13 +17,13 @@
   (let [proj-id (stg/create-project! db {:name "Quil"
                                          :user-id user-id})]
     (stg/create-action! db user-id {:project-id proj-id
-                                    :type :email
+                                    :type "email"
                                     :template "Quil {{VERSION}} was released. Go and update wiki and examples!"
                                     :address "nikelandjelo@gmail.com"
                                     :library "quil"
                                     :last-processed-version "2.2.2"})
     (stg/create-action! db user-id {:project-id proj-id
-                                    :type :email
+                                    :type "email"
                                     :template "ClojureScript {{VERSION}} was released. Go and update template!"
                                     :address "norg113@gmail.com"
                                     :library "org.clojure/clojurescript"

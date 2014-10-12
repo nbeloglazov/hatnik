f7b039680f56e4850b077c892080e0371b7aec3d
Norgat <norg113@gmail.com>
2014-10-12 10:22:44 +0400

    Add artifact select field to forms.

--- src/cljs/hatnik/web/client/form_components.cljs
+++ src/cljs/hatnik/web/client/form_components.cljs
@@ -53,49 +53,72 @@
   (state/set-current-email-template (.. e -target -value)))
 
 (defn email-action-form-body [data]
-  (dom/form
-   #js {:id "email-action-form"}
-   (dom/div #js {:className "form-group has-warning"
-                 :id "artifact-input-group"}
-            (dom/label #js {:for "artifact-input"} "Library")
-            (dom/input #js {:type "text"
-                            :className "form-control"
-                            :id "artifact-input"
-                            :placeholder "e.g. org.clojure/clojure"
-                            :onChange input-handle
-                            :value (-> data :ui :email-artifact-value)}))
-   (dom/div #js {:className "form-group"}
-            (dom/label #js {:for "emain-input"} "Email")
-            (dom/input #js {:type "email"
-                            :className "form-control"
-                            :id "emain-input"
-                            :value (-> data :data :user :email)
-                            :disabled "disabled"})
-            (dom/div #js {:className "form-group"}
-                     (dom/label #js {:for "emain-body-input"} "Email body")
-                     (dom/textarea #js {:cols "40"
-                                        :className "form-control"
-                                        :id "emain-body-input"
-                                        :value (-> data :ui :email-template)
-                                        :onChange email-template-handle})))))
+  (dom/div #js {:className "form-group"}
+           (dom/label #js {:for "emain-input"} "Email")
+           (dom/input #js {:type "email"
+                           :className "form-control"
+                           :id "emain-input"
+                           :value (-> data :data :user :email)
+                           :disabled "disabled"})
+           (dom/div #js {:className "form-group"}
+                    (dom/label #js {:for "emain-body-input"} "Email body")
+                    (dom/textarea #js {:cols "40"
+                                       :className "form-control"
+                                       :id "emain-body-input"
+                                       :value (-> data :ui :email-template)
+                                       :onChange email-template-handle}))))
 
-(defn email-create-action-body [data]
-  (email-action-form-body data))
+(defn artifact-input [data]
+  (dom/div #js {:className "form-group has-warning"
+                :id "artifact-input-group"}
+           (dom/label #js {:for "artifact-input"} "Library")
+           (dom/input #js {:type "text"
+                           :className "form-control"
+                           :id "artifact-input"
+                           :placeholder "e.g. org.clojure/clojure"
+                           :onChange input-handle
+                           :value (-> data :ui :email-artifact-value)})))
 
-(defn email-edit-action [data]
-  (email-action-form-body data))
+(defn action-type-change-handler [owner e]
+  (om/set-state! owner :action-type
+                 (keyword (.. e -target -value))))
+
+(defn action-type-selector [data owner]
+  (dom/div #js {:className "form-group"}
+           (dom/label nil "Action type")
+           (dom/select #js {:ref "action-type"
+                            :className "form-control"
+                            :onChange #(action-type-change-handler owner %)}
+                       (dom/option #js {:value "noop"} "Noop")
+                       (dom/option #js {:value "email"} "Email"))))
+
+(defn add-action-form-body [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:action-type :noop})
+    om/IRenderState
+    (render-state [this state]
+      (dom/form
+       #js {:id "email-action-form"}
+       (artifact-input data)
+       (action-type-selector data owner)
+       (let [action-type (:action-type state)]
+         (cond 
+          (= :email action-type) (email-action-form-body data)
+          (= :noop action-type) (dom/div nil "")
+          :else (dom/div nil "")))))))
+
 
 (def action-form-bodys 
-  {:email-action email-create-action-body
-   :email-edit-action email-edit-action})
+  {:email-action #(om/build add-action-form-body %)
+   :email-edit-action #(email-action-form-body %)})
 
 (defn action-form-body [data body]
   (reify
     om/IRender
     (render [this]
-      (apply 
-       (get action-form-bodys (-> data :ui :form-type))
-       [data]))))
+      (om/build add-action-form-body data))))
 
 ;; For selecting footer
 
@@ -137,10 +160,7 @@
       (dom/div 
        #js {:className "modal-dialog"}
        (dom/div #js {:className "modal-content"}
-                (dom/div #js {:className "modal-header"}
-                         (om/build action-form-header data))
-                (dom/div #js {:className "modal-body"}
-                         (om/build action-form-body data))
-                (dom/div #js {:className "modal-footer"}
-                         (om/build action-form-footer data))
+                (dom/div #js {:className "modal-header"} (om/build action-form-header data))
+                (dom/div #js {:className "modal-body"}  (om/build action-form-body data))
+                (dom/div #js {:className "modal-footer"} (om/build action-form-footer data))
                 )))))

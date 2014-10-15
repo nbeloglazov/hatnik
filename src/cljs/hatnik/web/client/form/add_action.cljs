7a184d74d5ebe6bdf97216cb73486bcc3007e746
Norgat <norg113@gmail.com>
2014-10-15 13:35:28 +0400

    Add artifact name input check.

--- src/cljs/hatnik/web/client/form/add_action.cljs
+++ src/cljs/hatnik/web/client/form/add_action.cljs
@@ -8,17 +8,47 @@
   (om/detach-root 
    (om/get-node component)))
 
+(defn library-check-handler [reply callback]
+  (let [resp (js->clj reply)]
+    (if (= "ok" (get resp "result"))
+      (callback "has-success" nil)
+      (callback "has-error" nil))))
+
+(defn check-input-value [data-handler check-handler timer new-val]
+  (js/clearTimeout timer)  
+  (check-handler 
+   "has-warning"
+   (js/setTimeout 
+    (fn []
+      (action/get-library new-val #(library-check-handler % check-handler)))
+    2000))
+  (data-handler new-val))
+
 (defn artifact-input-component [data owner]
   (reify
-    om/IRender
-    (render [this]
-      (dom/div #js {:className "form-group has-warning"
+    om/IInitState
+    (init-state [this]
+      {:timer nil
+       :form-status (if (= "" (:value data))
+                      "has-warning"
+                      "has-success")
+       :value (:value data)})
+
+    om/IRenderState
+    (render-state [this state]
+      (dom/div #js {:className (str "form-group " (:form-status state))
                     :id "artifact-input-group"}
                (dom/label #js {:htmlFor "artifact-input"} "Library")
                (dom/input #js {:type "text"
                                :className "form-control"
                                :placeholder "e.g. org.clojure/clojure"
-                               :onChange #((:handler data) (.. % -target -value))
+                               :onChange #(check-input-value 
+                                           (:handler data) 
+                                           (fn [x timer] 
+                                             (om/set-state! owner :form-status x)
+                                             (om/set-state! owner :timer timer))
+                                           (:timer state)
+                                           (.. % -target -value))
                                :value (:value data)})))))
 
 (defn user-email-component [data owner]

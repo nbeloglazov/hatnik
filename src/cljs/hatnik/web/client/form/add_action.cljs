c013f08466b48c4a76bb635de829ad12fcc77cd5
Norgat <norg113@gmail.com>
2014-10-27 12:13:28 +0300

    Remove artifact-input from add_action.cljs.

--- src/cljs/hatnik/web/client/form/add_action.cljs
+++ src/cljs/hatnik/web/client/form/add_action.cljs
@@ -5,54 +5,13 @@
             [hatnik.schema :as schm]
             [schema.core :as s])
   (:use [jayq.core :only [$]]
-        [clojure.string :only [split replace]]))
+        [clojure.string :only [split replace]]
+        [hatnik.web.client.form.artifact-input :only [artifact-input-component]]))
 
 (defn on-modal-close [component]  
   (om/detach-root 
    (om/get-node component)))
 
-(defn library-check-handler [reply callback]
-  (let [resp (js->clj reply)]
-    (if (= "ok" (get resp "result"))
-      (callback "has-success" nil)
-      (callback "has-error" nil))))
-
-(defn check-input-value [data-handler check-handler timer new-val]
-  (js/clearTimeout timer)  
-  (check-handler 
-   "has-warning"
-   (js/setTimeout 
-    (fn []
-      (action/get-library new-val #(library-check-handler % check-handler)))
-    1000))
-  (data-handler new-val))
-
-(defn artifact-input-component [data owner]
-  (reify
-    om/IInitState
-    (init-state [this]
-      {:timer nil
-       :form-status (if (= "" (:value data))
-                      "has-warning"
-                      "has-success")
-       :value (:value data)})
-
-    om/IRenderState
-    (render-state [this state]
-      (dom/div #js {:className (str "form-group " (:form-status state))
-                    :id "artifact-input-group"}
-               (dom/label #js {:htmlFor "artifact-input"} "Library")
-               (dom/input #js {:type "text"
-                               :className "form-control"
-                               :placeholder "e.g. org.clojure/clojure"
-                               :onChange #(check-input-value 
-                                           (:handler data) 
-                                           (fn [x timer] 
-                                             (om/set-state! owner :form-status x)
-                                             (om/set-state! owner :timer timer))
-                                           (:timer state)
-                                           (.. % -target -value))
-                               :value (:value data)})))))
 
 (defn option-element-map [val pred]
   (if pred

ef0e27ffd962c33ad1a28ff78dbc76f89b451e25
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-10-30 13:56:32 +0300

    Add spinner to artifact selection component.

--- src/cljs/hatnik/web/client/form/artifact_input.cljs
+++ src/cljs/hatnik/web/client/form/artifact_input.cljs
@@ -11,13 +11,14 @@
       (callback "has-success" (get resp "version") nil)
       (callback "has-error" nil nil))))
 
-(defn check-input-value [data-handler check-handler timer new-val]
+(defn check-input-value [data-handler check-handler started-handler timer new-val]
   (js/clearTimeout timer)
   (check-handler
    "has-warning"
    nil
    (js/setTimeout
     (fn []
+      (started-handler)
       (action/get-library new-val #(library-check-handler % check-handler)))
     1000))
   (data-handler new-val))
@@ -31,29 +32,33 @@
                       "has-warning"
                       "has-success")
        :value (:value data)
-       :version (:version data)})
+       :version (:version data)
+       :request-in-progress? false})
 
     om/IRenderState
     (render-state [this state]
       (dom/div #js {:className (str "form-group " (:form-status state))
                     :id "artifact-input-group"}
-               (dom/label #js {:htmlFor "artifact-input"
-                               :className "control-label"} "Library")
-               (dom/input #js {:type "text"
-                               :id "artifact-input"
-                               :className "form-control"
-                               :placeholder "e.g. org.clojure/clojure"
-                               :onChange #(check-input-value
-                                           (:handler data)
-                                           (fn [result version timer]
-                                             (om/set-state! owner :form-status result)
-                                             (om/set-state! owner :version version)
-                                             (om/set-state! owner :timer timer))
-                                           (:timer state)
-                                           (.. % -target -value))
-                               :value (:value data)})
+        (dom/label #js {:htmlFor "artifact-input"
+                        :className "control-label"} "Library")
+        (dom/input #js {:type "text"
+                        :id "artifact-input"
+                        :className "form-control"
+                        :placeholder "e.g. org.clojure/clojure"
+                        :onChange #(check-input-value
+                                    (:handler data)
+                                    (fn [result version timer]
+                                      (om/set-state! owner :form-status result)
+                                      (om/set-state! owner :version version)
+                                      (om/set-state! owner :timer timer)
+                                      (om/set-state! owner :request-in-progress? false))
+                                    (fn []
+                                      (om/set-state! owner :request-in-progress? true))
+                                    (:timer state)
+                                    (.. % -target -value))
+                        :value (:value data)})
                (dom/span #js {:className "form-control-feedback"}
-                         (case (:form-status state)
-                           "has-error" "Not found"
-                           "has-success" (:version state)
-                           nil))))))
+                 (cond (= (:form-status state) "has-error") "Not found"
+                       (= (:form-status state) "has-success") (:version state)
+                       (:request-in-progress? state) (dom/img #js {:src "/img/ajax-loader.gif"})
+                       :else nil))))))

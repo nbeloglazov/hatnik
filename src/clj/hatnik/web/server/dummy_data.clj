f3ca01bc58f329f36cd93fcc4b87c763b94d1e83
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-11-15 20:37:01 +0000

    Add pull request to dummy data.

--- src/clj/hatnik/web/server/dummy_data.clj
+++ src/clj/hatnik/web/server/dummy_data.clj
@@ -18,7 +18,24 @@
                                     :title "{{library}} {{version}} released"
                                     :body "Hey ho. {{library}} {{version}}, was {{previous-version}}."
                                     :library "org.clojure/clojure"
-                                    :last-processed-version "1.6.0"})))
+                                    :last-processed-version "1.6.0"})
+    (stg/create-action! db user-id {:project-id proj-id
+                                    :type "github-pull-request"
+                                    :repo "nbeloglazov/hatnik-test-lib"
+                                    :title "Update {{library}} to {{version}}"
+                                    :body "Results \n {{results-table}}"
+                                    :commit-message "Update {{library}} to {{version}}\n\nTest"
+                                    :operations [{:file "some/file"
+                                                  :regex "regex"
+                                                  :replacement "replacement"}
+                                                 {:file "project.clj"
+                                                  :regex "{{library}} \"[^\"]+\""
+                                                  :replacement "{{library}} \"{{version}}\""}
+                                                 {:file "README.md"
+                                                  :regex "hello"
+                                                  :replacement "world"}]
+                                    :library "compojure"
+                                    :last-processed-version "0.0.0"})))
 
 (defn create-quil-project [db user-id]
   (let [proj-id (stg/create-project! db {:name "Quil"

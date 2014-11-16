bb840def5d6c5902c84cdc312f1fd652e3d22619
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-11-16 22:03:19 +0000

    Add Google Analytics.

--- src/clj/hatnik/web/server/renderer.clj
+++ src/clj/hatnik/web/server/renderer.clj
@@ -1,15 +1,25 @@
 (ns hatnik.web.server.renderer
   (:require [hiccup.core :as hc]))
 
+(def ga-script
+  [:script
+   "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51485241-3', 'auto');
+  ga('send', 'pageview');"])
+
 (defn page-html-head []
   (hc/html
    [:head
     [:title "Hatnik"]
     [:meta {:name "viewport"
             :content "width=device-width, initial-scale=1.0"}]
+    ga-script
     [:link {:rel "stylesheet" :href "/css/bootstrap.min.css"}]
     [:link {:rel "stylesheet" :href "/css/styles.css"}]]))
-
 
 (defn github-link [config]
   (str "https://github.com/login/oauth/authorize?"

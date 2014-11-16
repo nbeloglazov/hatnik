383c2cfa18ce6d29417e52f6e353a7906884d643
Norgat <norg113@gmail.com>
2014-11-16 14:12:56 +0300

    Fix github pull request status on main page.

--- src/clj/hatnik/web/server/renderer.clj
+++ src/clj/hatnik/web/server/renderer.clj
@@ -110,7 +110,7 @@
      "have a place that lists the latest versions for selected libraries."]
     [:li.available {:title "Work in progress"} [:strong "GitHub Issue"]
      " - create an issue in the selected repo. The text can be customized just like in email."]
-    [:li.unavailable {:title "Work in progress"} [:strong "GitHub Pull Request"]
+    [:li.available {:title "Work in progress"} [:strong "GitHub Pull Request"]
      " - create a pull request in selected repo using simple find-and-replace-in-file operations."]
     [:li.unavailable {:title "Work in progress"} [:strong "GitHub Wiki"]
      " - update a wiki page using the same find-and-replace operations."]]

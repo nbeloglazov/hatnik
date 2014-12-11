12e1e267145bd052c360149535f4bfc04d370be3
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-12-10 19:50:44 -0800

    Fix backup tool by updating to latest version of google API.

--- tools/backup/project.clj
+++ tools/backup/project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.google.apis/google-api-services-storage "v1beta2-rev43-1.18.0-rc"]
-                 [com.google.http-client/google-http-client-jackson2 "1.17.0-rc"]
-                 [com.google.oauth-client/google-oauth-client-jetty "1.17.0-rc"]]
+                 [com.google.apis/google-api-services-storage "v1-rev22-1.19.0"]
+                 [com.google.http-client/google-http-client-jackson2 "1.19.0"]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.19.0"]]
 
   :main hatnik.tools.backup
   :aot :all)

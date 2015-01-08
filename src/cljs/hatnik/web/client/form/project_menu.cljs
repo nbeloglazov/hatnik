4ba10e22df748146fb06b315bdb0a6354876537d
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-01-07 21:55:22 -0800

    Refactor project menu, split big method into smaller chunks.

--- src/cljs/hatnik/web/client/form/project_menu.cljs
+++ src/cljs/hatnik/web/client/form/project_menu.cljs
@@ -3,9 +3,49 @@
             [om.dom :as dom :include-macros true]
             [hatnik.web.client.z-actions :as action]
             [hatnik.web.client.utils :as u]
-            [hatnik.schema :as schm])
-  (:use [jayq.core :only [$]]))
+            [hatnik.schema :as schm]
+            [jayq.core :refer [$]]))
 
+(defn header [data]
+  (dom/div
+   #js {:className "modal-header"}
+   (dom/h4 #js {:className "modal-title"}
+           (if (:project-id data)
+             "Update project"
+             "Create project")
+           (when (u/mobile?)
+             (dom/button
+              #js {:className "btn btn-default close-btn"
+                   :onClick #(.modal ($ :#iModalProjectMenu) "hide")}
+              "Close")))))
+
+(defn body [data]
+  (dom/div
+   #js {:className "modal-body"}
+   (dom/form #js {:className "form-horizontal"}
+             (u/form-field {:data data
+                            :field :name
+                            :title "Name"
+                            :type :text
+                            :validator (schm/string-of-length 1 128)}))))
+
+(defn footer [data]
+  (let [id (:project-id data)
+        name (:name data)]
+    (dom/div
+     #js {:className "modal-footer"}
+     (dom/div
+      #js {:className "btn btn-primary pull-left"
+           :onClick (if id
+                      #(action/update-project id name)
+                      #(action/send-new-project-request name))}
+      (if id
+        "Update"
+        "Create"))
+     (when id
+       (dom/div #js {:className "btn btn-danger pull-right"
+                     :onClick #(action/delete-project id)}
+                "Delete")))))
 
 (defn- project-menu [data owner]
   (reify
@@ -15,46 +55,13 @@
 
     om/IRender
     (render [this]
-      (let [name (:name data)
-            id (:project-id data)]
-        (dom/div
-         #js {:className "modal-dialog"}
-         (dom/div
-          #js {:className "modal-content"}
-          (dom/div #js {:className "modal-header"}
-                   (dom/h4 #js {:className "modal-title"}
-                           (if id
-                             "Update project"
-                             "Create project")
-                           (when (u/mobile?)
-                             (dom/button
-                              #js {:className "btn btn-default close-btn"
-                                   :onClick #(.modal ($ :#iModalProjectMenu) "hide")}
-                              "Close"))))
-
-          (dom/div
-           #js {:className "modal-body"}
-           (dom/form nil
-                     (dom/div #js {:className (str "form-group "
-                                                   (u/validate (schm/string-of-length 1 128) name))}
-                              (dom/input #js {:className "form-control"
-                                              :type "text"
-                                              :value name
-                                              :onChange #(om/update! data :name (u/ev-value %))}))))
-
-          (dom/div #js {:className "modal-footer"}
-
-                   (dom/div #js {:className "btn btn-primary pull-left"
-                                 :onClick (if id
-                                            #(action/update-project id name)
-                                            #(action/send-new-project-request name))}
-                            (if id
-                              "Update"
-                              "Create"))
-                   (when id
-                     (dom/div #js {:className "btn btn-danger pull-right"
-                                   :onClick #(action/delete-project id)}
-                              "Delete")))))))))
+      (dom/div
+       #js {:className "modal-dialog"}
+       (dom/div
+        #js {:className "modal-content"}
+        (header data)
+        (body data)
+        (footer data))))))
 
 (defn show [project]
   (om/root project-menu project

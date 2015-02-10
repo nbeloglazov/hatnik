daadec65f0a178cac110a6731def1c8b1cad4b51
Hatnik <support@hatnik.com>
2015-02-10 01:27:48 +0000

    Update org.clojure/clojurescript to 0.0-2814

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  [commons-io "2.4"]
 
                  ; ClojureScript
-                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/clojurescript "0.0-2814"]
                  [jayq "2.5.4"]
                  [org.omcljs/om "0.8.8"]]
 

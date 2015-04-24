be93168d047f3356ba4e23962286e3dcf3d2553d
Hatnik <support@hatnik.com>
2015-04-24 18:49:27 +0000

    Update org.clojure/clojure to 1.7.0-beta2

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Web app for tracking library releases."
   :url "http://hatnik.com"
   :dependencies [; Clojure
-                 [org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojure "1.7.0-beta2"]
                  [ring "1.4.0-beta1"]
                  [compojure "1.3.3"]
                  [hiccup "1.0.5"]

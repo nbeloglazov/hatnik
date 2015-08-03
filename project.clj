b9889594a6e98f35c45b743f47ea43f31361141f
Nikita Beloglazov <nikelandjelo@gmail.com>
2015-08-02 17:06:34 -0700

    Update webdriver deps.

--- project.clj
+++ project.clj
@@ -42,9 +42,9 @@
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]
                         [com.cemerick/piggieback "0.2.1"]
-                        [org.seleniumhq.selenium/selenium-java "2.46.0"]
-                        [org.seleniumhq.selenium/selenium-remote-driver "2.46.0"]
-                        [org.seleniumhq.selenium/selenium-server "2.46.0"]]
+                        [org.seleniumhq.selenium/selenium-java "2.47.1"]
+                        [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]
+                        [org.seleniumhq.selenium/selenium-server "2.47.1"]]
 
          :plugins [[jonase/eastwood "0.2.1"]
                    [com.keminglabs/cljx "0.5.0" :exclusions [org.clojure/clojure]]]
